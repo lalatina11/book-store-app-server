8af115f1c40383dd1b99aeb54c1bfb36d7a7df54
lalatina11 <rahmadancandra@gmail.com>
2025-08-19 11:38:44 +0700

    feat: add create user repository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,8 +1,12 @@
 import type {UserFields} from "../types";
+import User from "../db/models/User.ts";
 
 const UserRepository = {
     create: async (fields: UserFields) => {
-
+        const {username, email, password, avatar} = fields;
+        const newUser = new User({email, password, username, avatar})
+        const user= await newUser.save()
+        return user.toObject()
     },
 }
 
