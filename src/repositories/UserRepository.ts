4dbb95020fbf68a88db5467afd8b0799ae2ea992
lalatina11 <rahmadancandra@gmail.com>
2025-08-19 19:11:31 +0700

    feat: add create user find unique func

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,12 +2,32 @@ import type {UserFields} from "../types";
 import User from "../db/models/User.ts";
 
 const UserRepository = {
-    create: async (fields: UserFields) => {
+    create: async (fields: UserFields & { password: string }) => {
         const {username, email, password, avatar} = fields;
         const newUser = new User({email, password, username, avatar})
-        const user= await newUser.save()
+        const user = await newUser.save()
         return user.toObject()
     },
+    findById: async (id: string) => {
+        const user = await User.findById({id})
+        if (!user) return null
+        return user.toObject()
+    },
+    findByUsername: async (username: string) => {
+        const user = await User.findOne({username})
+        if (!user) return null
+        return user.toObject()
+    },
+    findByEmail: async (email: string) => {
+        const user = await User.findOne({email})
+        if (!user) return null
+        return user.toObject()
+    },
+    findByUsernameOrEmail: async (identifier: string) => {
+        const user = await User.findOne({$or: [{username: identifier}, {email: identifier}]})
+        if (!user) return null
+        return user.toObject()
+    }
 }
 
 export default UserRepository
