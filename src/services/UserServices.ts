401f9409ba364372344b977a81148e05a910be29
lalatina11 <rahmadancandra@gmail.com>
2025-08-19 11:38:54 +0700

    feat: add create user services

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,9 +1,16 @@
 import type {UserFields} from "../types";
+import {hashSync} from "bcrypt-ts";
+import UserRepository from "../repositories/UserRepository.ts";
 
 const UserServices = {
     create: async (fields: UserFields) => {
-        const {username, email, password} = fields;
-        return {username, email, password};
+        const {username, email, password: passwordFromInput} = fields;
+        if (!passwordFromInput) {
+            throw new Error("Password is required!");
+        }
+        const password = hashSync(passwordFromInput, 10);
+        const avatar = "https://api.dicebear.com/9.x/adventurer/svg?seed=" + username;
+        return await UserRepository.create({username, email, password, avatar});
     },
 }
 
